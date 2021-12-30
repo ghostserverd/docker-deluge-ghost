21b1ff9a2951b3a74e75c841f14dd3a3f4d4caaf
ghostserverd <42226464+ghostserverd@users.noreply.github.com>
2021-12-30 14:28:25 -0800

    Update deluge-postprocess.sh

--- deluge-postprocess.sh
+++ deluge-postprocess.sh
@@ -4,45 +4,45 @@
 TR_TORRENT_DIR="$3"
 TR_TORRENT_NAME="$2"
 
-PATH_REPLACE="/downloads/"
-PATH_REPLACE_WITH="/downloads/deluge/"
+PATH_REPLACE="downloads"
+PATH_REPLACE_WITH="downloads/deluge"
 
-# ARG_PATH=$(echo "${TR_TORRENT_DIR/"PATH_REPLACE"/"$PATH_REPLACE_WITH"}/$TR_TORRENT_NAME")
-ARG_PATH="$TR_TORRENT_DIR"
+ARG_PATH=$(echo "${TR_TORRENT_DIR/$PATH_REPLACE/$PATH_REPLACE_WITH}/$TR_TORRENT_NAME")
+# ARG_PATH="$TR_TORRENT_DIR"
 ARG_NAME="$TR_TORRENT_NAME"
 ARG_LABEL="N/A"
 
 # Configuration
 CONFIG_OUTPUT="/media"
 FILEBOT_PORT=${FILEBOT_PORT:-7676}
-IGNORE_LABELS=${IGNORE_LABELS:-""}
+# IGNORE_LABELS=${IGNORE_LABELS:-""}
 
-SONARR_CATEGORY=${SONARR_CATEGORY:-"sonarr"}
-SONARR_PORT=${SONARR_PORT:-""}
-SONARR_API_KEY=${SONARR_API_KEY:-""}
+# SONARR_CATEGORY=${SONARR_CATEGORY:-"sonarr"}
+# SONARR_PORT=${SONARR_PORT:-""}
+# SONARR_API_KEY=${SONARR_API_KEY:-""}
 
-RADARR_CATEGORY=${RADARR_CATEGORY:-"radarr"}
-RADARR_PORT=${RADARR_PORT:-""}
-RADARR_API_KEY=${RADARR_API_KEY:-""}
+# RADARR_CATEGORY=${RADARR_CATEGORY:-"radarr"}
+# RADARR_PORT=${RADARR_PORT:-""}
+# RADARR_API_KEY=${RADARR_API_KEY:-""}
 
-LIDARR_CATEGORY=${LIDARR_CATEGORY:-"lidarr"}
-LIDARR_PORT=${LIDARR_PORT:-""}
-LIDARR_API_KEY=${LIDARR_API_KEY:-""}
+# LIDARR_CATEGORY=${LIDARR_CATEGORY:-"lidarr"}
+# LIDARR_PORT=${LIDARR_PORT:-""}
+# LIDARR_API_KEY=${LIDARR_API_KEY:-""}
 
-FILEBOT_LABEL=$ARG_LABEL
-case $TR_TORRENT_DIR in
-    *$SONARR_CATEGORY*)
-        FILEBOT_LABEL="tv"
-    ;;
+# FILEBOT_LABEL=$ARG_LABEL
+# case $TR_TORRENT_DIR in
+#     *$SONARR_CATEGORY*)
+#         FILEBOT_LABEL="tv"
+#     ;;
 
-    *$RADARR_CATEGORY*)
-        FILEBOT_LABEL="movie"
-    ;;
+#     *$RADARR_CATEGORY*)
+#         FILEBOT_LABEL="movie"
+#     ;;
 
-    *$LIDARR_CATEGORY*)
-        FILEBOT_LABEL="music"
-    ;;
-esac
+#     *$LIDARR_CATEGORY*)
+#         FILEBOT_LABEL="music"
+#     ;;
+# esac
 
 set | grep \
         -e TR_TORRENT_DIR \
@@ -66,7 +66,7 @@ set | grep \
         >> /config/deluge_env.log
 
 # exit with success if label should not be processed by filebot
-[ ! -z "$IGNORE_LABELS" ] && case $FILEBOT_LABEL in $IGNORE_LABELS) exit 0;; esac
+# [ ! -z "$IGNORE_LABELS" ] && case $FILEBOT_LABEL in $IGNORE_LABELS) exit 0;; esac
 
 FILEBOT_CMD=$(\
 echo curl \
@@ -78,39 +78,39 @@ echo curl \
 echo $FILEBOT_CMD >> /config/filebot.log
 eval $FILEBOT_CMD
 
-REFRESH_NAME=""
-REFRESH_URL=""
+# REFRESH_NAME=""
+# REFRESH_URL=""
 
-case $TR_TORRENT_DIR in
-    *$SONARR_CATEGORY*)
-        if [ $SONARR_PORT != "" ] && [ $SONARR_API_KEY != "" ]; then
-            REFRESH_NAME="RescanSeries"
-            REFRESH_URL="http://sonarr:${SONARR_PORT}/api/command?apikey=${SONARR_API_KEY}"
-        fi
-    ;;
+# case $TR_TORRENT_DIR in
+#     *$SONARR_CATEGORY*)
+#         if [ $SONARR_PORT != "" ] && [ $SONARR_API_KEY != "" ]; then
+#             REFRESH_NAME="RescanSeries"
+#             REFRESH_URL="http://sonarr:${SONARR_PORT}/api/command?apikey=${SONARR_API_KEY}"
+#         fi
+#     ;;
 
-    *$RADARR_CATEGORY*)
-        if [ $RADARR_PORT != "" ] && [ $RADARR_API_KEY != "" ]; then
-            REFRESH_NAME="RescanMovie"
-            REFRESH_URL="http://radarr:${RADARR_PORT}/api/command?apikey=${RADARR_API_KEY}"
-        fi
-    ;;
+#     *$RADARR_CATEGORY*)
+#         if [ $RADARR_PORT != "" ] && [ $RADARR_API_KEY != "" ]; then
+#             REFRESH_NAME="RescanMovie"
+#             REFRESH_URL="http://radarr:${RADARR_PORT}/api/command?apikey=${RADARR_API_KEY}"
+#         fi
+#     ;;
 
-    *$LIDARR_CATEGORY*)
-        if [ $LIDARR_PORT != "" ] && [ $LIDARR_API_KEY != "" ]; then
-            REFRESH_NAME="RescanArtist"
-            REFRESH_URL="http://lidarr:${LIDARR_PORT}/api/v1/command?apikey=${LIDARR_API_KEY}"
-        fi
-    ;;
-esac
+#     *$LIDARR_CATEGORY*)
+#         if [ $LIDARR_PORT != "" ] && [ $LIDARR_API_KEY != "" ]; then
+#             REFRESH_NAME="RescanArtist"
+#             REFRESH_URL="http://lidarr:${LIDARR_PORT}/api/v1/command?apikey=${LIDARR_API_KEY}"
+#         fi
+#     ;;
+# esac
 
-if [ $REFRESH_URL != "" ]; then
-    REFRESH_CMD=$(\
-        echo curl \
-            -d \"{\\\"name\\\":\\\"${REFRESH_NAME}\\\"}\" \
-            -H \"Content-Type: application/json\" \
-      -X POST \
-            ${REFRESH_URL})
-    echo $REFRESH_CMD >> /config/pvr-refresh.log
-    eval $REFRESH_CMD
-fi
+# if [ $REFRESH_URL != "" ]; then
+#     REFRESH_CMD=$(\
+#         echo curl \
+#             -d \"{\\\"name\\\":\\\"${REFRESH_NAME}\\\"}\" \
+#             -H \"Content-Type: application/json\" \
+#       -X POST \
+#             ${REFRESH_URL})
+#     echo $REFRESH_CMD >> /config/pvr-refresh.log
+#     eval $REFRESH_CMD
+# fi
